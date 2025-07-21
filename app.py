2ca7e03e40ecb1e13a3355c0a45b714e2549b1de
Liteboho <deemohapi470@gmail.com>
2025-07-21 11:08:05 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from dotenv import load_dotenv
+load_dotenv()
+
+import smtplib, os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+app = Flask(__name__, static_folder="static", template_folder="templates")
+CORS(app)
+
+EMAIL = os.getenv("EMAIL")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+TO_EMAIL = os.getenv("TO_EMAIL") or EMAIL
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/software")
+def software():
+    return render_template("software.html")
+
+@app.route("/hr")
+def hr():
+    return render_template("hr.html")
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+@app.route('/google.html')
+def google_verification():
+    return app.send_static_file('google.html')
